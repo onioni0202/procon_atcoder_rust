dc027f1ee0639034c969ec7961c9ab99293a4325
onioni0202 <49858062+onioni0202@users.noreply.github.com>
2022-03-19 11:04:19 +0900

    solved abc243-c

--- abc243/src/bin/c.rs
+++ abc243/src/bin/c.rs
@@ -1,5 +1,22 @@
 use proconio::input;
+use proconio::marker::Chars;
+use std::collections::HashMap;
 
 fn main() {
-
+    input! {
+        n : usize,
+        xy : [(i32, i32); n],
+        s : Chars,
+    }
+    let mut map: HashMap<i32, Vec<(i32, usize)>> = HashMap::new();
+    for i in 0..n {
+        let (x, y) = xy[i];
+        (*map.entry(y).or_insert(vec![])).push((x, i));
+    }
+    let mut ans = false;
+    for (_, mut xs) in map {
+        xs.sort();
+        ans |= xs.windows(2).any(|j| s[j[0].1] == 'R' && s[j[1].1] == 'L');
+    }
+    println!("{}", if ans { "Yes" } else { "No" });
 }
