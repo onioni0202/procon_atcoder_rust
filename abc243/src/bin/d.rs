5e5611a8838b848e5424a9b667012be97e6fe324
onioni0202 <49858062+onioni0202@users.noreply.github.com>
2022-03-20 11:57:47 +0900

    solved abc243-d

--- abc243/src/bin/d.rs
+++ abc243/src/bin/d.rs
@@ -1,5 +1,23 @@
 use proconio::input;
+use proconio::marker::Chars;
 
 fn main() {
+    input! {
+        n: usize,
+        mut x: i64,
+        s: Chars,
+    }
+    let mut bin = format!("{:b}", x).chars().collect::<Vec<char>>();
 
+    for i in 0..n {
+        let chr = s[i];
+        match chr {
+            'U' => {bin.pop();},
+            'L' => {bin.push('0');},
+            'R' => {bin.push('1');},
+            _ => {unreachable!()},
+        }
+    }
+    let str = bin.iter().collect::<String>();
+    println!("{}", i64::from_str_radix(&str, 2).unwrap());
 }
