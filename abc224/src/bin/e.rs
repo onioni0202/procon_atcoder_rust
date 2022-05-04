d46028a9e91cecd55acbaf49b6c77427437d8075
onioni0202 <49858062+onioni0202@users.noreply.github.com>
2022-05-04 23:30:09 +0900

    solved abc224-e,f

--- abc224/src/bin/e.rs
+++ abc224/src/bin/e.rs
@@ -1,5 +1,33 @@
 use proconio::input;
+use proconio::marker::Usize1;
+use std::collections::BTreeMap;
 
 fn main() {
-
+    input! {
+        H: usize,
+        W: usize,
+        N: usize,
+        rca: [(Usize1, Usize1, i32); N]
+    }
+    let mut bmap = BTreeMap::<i32, Vec<usize>>::new();
+    for (idx, (_, _, a)) in rca.iter().enumerate() {
+        bmap.entry(-*a).or_insert(vec![]).push(idx);
+    }
+    let mut maxrow = vec![-1; H];
+    let mut maxcol = vec![-1; W];
+    let mut ans = vec![0; N];
+    for (_, indice) in bmap.iter() {
+        for &idx in indice {
+            let (r, c) = (rca[idx].0, rca[idx].1);
+            ans[idx] = i32::max(maxrow[r], maxcol[c]) + 1;
+        }
+        for &idx in indice {
+            let (r, c) = (rca[idx].0, rca[idx].1);
+            maxrow[r] = i32::max(maxrow[r], ans[idx]);
+            maxcol[c] = i32::max(maxcol[c], ans[idx]);
+        }
+    }
+    for idx in 0..N {
+        println!("{}", ans[idx]);
+    }
 }
--- abc224/src/bin/f.rs
+++ abc224/src/bin/f.rs
@@ -1,5 +1,44 @@
 use proconio::input;
+use proconio::marker::Chars;
+
+const MOD: i64 = 998244353;
+
+fn modinv(mut a: i64, m: i64) -> i64 {
+    let mut b: i64 = m;
+    let mut u: i64 = 1;
+    let mut v: i64 = 0;
+    while b > 0 {
+        let t = a / b;
+        a -= t * b;
+        std::mem::swap(&mut a, &mut b);
+        u -= t * v;
+        std::mem::swap(&mut u, &mut v);
+    }
+    u %= m;
+    if u < 0 {
+        u += m;
+    }
+    u
+}
 
 fn main() {
-
+    input! {
+        mut s: Chars,
+    }
+    s.reverse();
+    let n = s.len();
+    let mut ans: i64 = 0;
+    let mut c: i64 = 1;
+    let inv2 = modinv(2, MOD);
+    for chr in s {
+        ans += c * (chr as i64 - '0' as i64);
+        c = (c * 10 + 1) * inv2;
+        ans %= MOD;
+        c %= MOD;
+    }
+    for _ in 0..n - 1 {
+        ans *= 2;
+        ans %= MOD;
+    }
+    println!("{:?}", ans);
 }
