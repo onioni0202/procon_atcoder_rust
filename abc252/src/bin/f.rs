6b2ca887fc881e90f9ac9eec27b20efd33a92ec4
onioni0202 <49858062+onioni0202@users.noreply.github.com>
2022-05-24 17:19:00 +0900

    solved abc252-f

--- abc252/src/bin/f.rs
+++ abc252/src/bin/f.rs
@@ -1,5 +1,26 @@
 use proconio::input;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 
 fn main() {
-    
+    input! {
+        n: usize,
+        l: i64,
+        a: [i64; n]
+    }
+    let mut heapq = BinaryHeap::new();
+    let mut sum = 0;
+    for a_v in a {
+        heapq.push(Reverse(a_v));
+        sum += a_v;
+    }
+    if l - sum > 0 {
+        heapq.push(Reverse(l - sum));
+    }
+    let mut ans = 0;
+    while let (Some(Reverse(v1)), Some(Reverse(v2))) = (heapq.pop(), heapq.pop()) {
+        ans += v1 + v2;
+        heapq.push(Reverse(v1 + v2));
+    }
+    println!("{}", ans);
 }
